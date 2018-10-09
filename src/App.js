448e9d155a69c075c5ade7b980fca913b4aaed18
KillerTwo <lwt2710@163.com>
2018-10-09 17:07:26 +0800

    first commit ...

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const electron = window.electron;
+const app = electron.app;
 class App extends Component {
+
+  
+  handleClose = ()=>{
+    console.log('退出应用。');
+    electron.ipcRenderer.on('born', (event, person) => {console.log(person, 'born')});
+    // electron.app.quit();
+    // app.quit();
+    // electron.dialog.showOpenDialog({properties: ['openFile', 'openDirectory', 'multiSelections']})
+    electron.ipcRenderer.send('create', 'snowtest');
+    console.log("snow","11111");
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +34,9 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
+        <h1>hello world...</h1>
+        <button onClick={this.handleClose}>点击关闭应用</button>
       </div>
     );
   }
